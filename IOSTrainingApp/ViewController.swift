564e49e743a9973e64a5929994341da58a6d1d66
Omar Bizreh <omarbizrehxda@gmail.com>
2016-02-06 13:29:58 +0200

    Initial Commit

---
+++ IOSTrainingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSTrainingApp
+//
+//  Created by Omar Bizreh on 2/6/16.
+//  Copyright © 2016 Omar Bizreh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
