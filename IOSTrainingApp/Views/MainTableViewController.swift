1f454925fdedbce8877a0478d0daccc55650238e
Omar Bizreh <omarbizrehxda@gmail.com>
2016-02-07 23:01:35 +0200

    Include search bar and example of Searchbar scopes

--- IOSTrainingApp/Views/MainTableViewController.swift
+++ IOSTrainingApp/Views/MainTableViewController.swift
@@ -9,18 +9,21 @@
 import UIKit
 import CoreData
 
-class MainTableViewController: UITableViewController {
-
+class MainTableViewController: UITableViewController, UISearchResultsUpdating {
+    
     var Names: [String] = [String]()
     var people: [NSManagedObject] = [NSManagedObject]()
+    var filteredPeople: [NSManagedObject] = [NSManagedObject]()
     let cellIdentifier = "reuseIdentifier"
+    let searchController: UISearchController = UISearchController(searchResultsController: nil)
+    let searchScopes: [String] = ["First Name", "Last Name"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         let btnRefresh = UIBarButtonItem(title: "Refresh", style: UIBarButtonItemStyle.Plain, target: self, action: "loadData")
@@ -28,13 +31,37 @@ class MainTableViewController: UITableViewController {
         let btnAdd = UIBarButtonItem(title: "Add", style: UIBarButtonItemStyle.Done, target: self, action: "AddName")
         self.navigationItem.leftBarButtonItem = btnAdd
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.definesPresentationContext = true
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.scopeButtonTitles = self.searchScopes
+        tableView.tableHeaderView = searchController.searchBar
     }
-
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else{
+            return
+        }
+        self.filterContentForSearchText(text, scope: self.searchController.searchBar.scopeButtonTitles![self.searchController.searchBar.selectedScopeButtonIndex])
+    }
+    
+    func filterContentForSearchText(searchText: String, scope: String = "Both"){
+        self.filteredPeople = self.people.filter({ (object) -> Bool in
+            if scope == self.searchScopes[0]{
+                return (object.valueForKey("name") as! String).lowercaseString.containsString(searchText.lowercaseString)
+            }
+            else{
+                return false
+            }
+        })
+        self.tableView.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
     func loadData(){
         self.fetchPersons()
@@ -95,51 +122,57 @@ class MainTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        if self.searchController.active && self.searchController.searchBar.text != ""{
+            return self.filteredPeople.count
+        }
         return self.people.count
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier, forIndexPath: indexPath)
-
+        
         //cell.textLabel?.text = self.Names[indexPath.row]
-
-        cell.textLabel?.text = self.people[indexPath.row].valueForKey("name") as? String
+        
+        if self.searchController.active && self.searchController.searchBar.text != ""{
+            cell.textLabel?.text = self.filteredPeople[indexPath.row].valueForKey("name") as? String
+        }else{
+            cell.textLabel?.text = self.people[indexPath.row].valueForKey("name") as? String
+        }
         
         return cell
     }
     
-
+    
     
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
-
+    
+    
     /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
     }
     */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
     }
     */
-
+    
     
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
@@ -147,15 +180,15 @@ class MainTableViewController: UITableViewController {
         return true
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
