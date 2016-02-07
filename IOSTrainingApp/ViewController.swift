8802e54cd5ffe25d65c4c7c6c2a34a604a9e1215
Omar Bizreh <omarbizrehxda@gmail.com>
2016-02-07 18:52:31 +0200

    More code for navigation drawer

--- IOSTrainingApp/ViewController.swift
+++ IOSTrainingApp/ViewController.swift
@@ -7,20 +7,62 @@
 //
 
 import UIKit
+import KYDrawerController
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var lblTitle: UILabel!
+    @IBOutlet var tableView: UITableView!
     @IBOutlet var viewHeader: UIView!
+    private let cellIdentifier = "reuseCell"
+    private let headersArray: [String] = ["Controls", "General"]
+    private var drawerView: KYDrawerController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //self.viewHeader.backgroundColor = UIColor(patternImage: UIImage(named: "header_image.jpg")!)
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
+        self.drawerView = (UIApplication.sharedApplication().windows[0].rootViewController as! KYDrawerController)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 3
+        }else{
+            return 2
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier)
+        if indexPath.section == 0 {
+            cell?.textLabel?.text = "Hellow World"
+        }
+        else{
+            cell?.textLabel?.text = "Hello World"
+        }
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.drawerView?.setDrawerState(.Closed, animated: true)
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.headersArray[section]
+    }
+    
 }
 
